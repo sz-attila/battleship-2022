fb15eba97928f3da866912a8b17d9dc2ec46ab43
szbence71 <78900051+szbence71@users.noreply.github.com>
2022-12-23 01:19:31 +0100

    Rules Created

---
+++ Battleship/Battleship/Rules.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Battleship
+{
+    /// <summary>
+    /// A class which contains common methods for a game.
+    /// </summary>
+    public static class Rules
+    {
+        public static void EndGame(string player1, string player2, int rounds, int player1Hits, int player2Hits, string winner)
+        {
+            _ = MessageBox.Show("Congratulations! {0} won!", winner);
+            DbHelper.InsertToDb(player1, player2, rounds, player1Hits, player2Hits, winner);
+        }
+    }
+}
